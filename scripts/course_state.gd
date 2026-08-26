191c393d5faf764538f030ab007d407ab758cc7d
Cole Stevenson <colerjstevenson@gmail.com>
2026-08-25 21:14:50 -0400

    start scene polish

--- scripts/course_state.gd
+++ scripts/course_state.gd
@@ -2,14 +2,19 @@ extends Node
 
 var hole_strokes: int = 0
 var total_strokes: int = 0
+var counting_enabled: bool = false
 
 func reset_round() -> void:
 	hole_strokes = 0
 	total_strokes = 0
+	counting_enabled = false
 
 func reset_hole() -> void:
 	hole_strokes = 0
+	counting_enabled = true
 
 func register_shot() -> void:
+	if not counting_enabled:
+		return
 	hole_strokes += 1
 	total_strokes += 1
